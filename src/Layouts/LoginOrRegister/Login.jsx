2ad187c7ab7477eb51ac4eea4c75dd286c73ed00
Sadi Muhammad Tanzim <saditanzim@gmail.com>
2024-05-10 22:07:42 +0600

    login page added

--- src/Layouts/LoginOrRegister/Login.jsx
+++ src/Layouts/LoginOrRegister/Login.jsx
@@ -1,9 +1,66 @@
+import { Button, Divider, Input } from 'antd';
 import React from 'react';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
+
     return (
-        <div>
-            this is login
+        <div className='flex border mt-5 w-[80%] mx-auto min-h-[550px] rounded-lg'>
+            <div className='border-r bg-teal-500 text-center text-white w-[40%] flex justify-center items-center rounded-l-lg'>
+                <div className='space-y-5'>
+                    <h1 className='text-3xl font-bold'>Hello, Friend!</h1>
+                    <p className=''>Enter Your Personal Details <br /> and  start journey with us</p>
+                    <div>
+                        <Link to="/register"><button className='border px-10 py-3 rounded-full'>Sign Up</button></Link>
+                    </div>
+                </div>
+            </div>
+
+            <div className='flex items-center justify-center grow'>
+                <div className='space-y-5'>
+                    <h1 className='text-center text-3xl text-teal-500 font-semibold'>Sign In to BD Services</h1>
+                    <div className='flex justify-center'>
+                        <Button type="" shape="circle">
+                            G
+                        </Button>
+                    </div>
+
+                    <Divider>OR</Divider>
+
+                    <Input
+                        className='py-3'
+                        placeholder="Email"
+                        prefix={
+                            <UserOutlined
+                                style={{
+                                    color: 'rgba(0,0,0,.25)',
+                                }}
+                            />
+                        }
+                    />
+
+
+                    <Input.Password
+                        className='py-3'
+                        placeholder="Password"
+                        prefix={
+                            <LockOutlined
+                                style={{
+                                    color: 'rgba(0,0,0,.25)',
+                                }}
+                            />
+                        }
+
+                    />
+
+
+                    <div className='flex justify-center'>
+                        <button className='bg-teal-500 text-white px-10 py-3 rounded-full'>Sign In</button>
+                    </div>
+
+                </div>
+            </div>
         </div>
     );
 };
