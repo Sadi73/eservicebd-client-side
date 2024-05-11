eac3a49ddc195e716f6e7da84d2ad554401c1bc3
Sadi Muhammad Tanzim <saditanzim@gmail.com>
2024-05-11 22:31:00 +0600

    modified styles

--- src/Layouts/BookService/BookService.jsx
+++ src/Layouts/BookService/BookService.jsx
@@ -13,17 +13,27 @@ const BookService = ({ isModalOpen, setIsModalOpen }) => {
     return (
         <>
             <Modal
-                title="Basic Modal"
+                title="Book Service"
                 open={isModalOpen}
                 onOk={handleOk}
                 onCancel={handleCancel}
                 footer={null}
-
+                width={700}
             >
                 <form className=' mx-auto space-y-3 my-10'>
                     <div>
-                        <label htmlFor="">Service Title</label>
+                        <label htmlFor="">Service ID</label>
+                        <input type="text" name="" id="" className='border w-full py-2 pl-2' placeholder='Service ID' />
+                    </div>
+
+                    <div>
+                        <label htmlFor="">Service Name</label>
                         <input type="text" name="" id="" className='border w-full py-2 pl-2' placeholder='Service Title' />
+                    </div>
+
+                    <div>
+                        <label htmlFor="">Service ImageURL</label>
+                        <input type="text" name="" id="" className='border w-full py-2 pl-2' placeholder='Service ImageURL' />
                     </div>
 
                     <div>
@@ -32,13 +42,23 @@ const BookService = ({ isModalOpen, setIsModalOpen }) => {
                     </div>
 
                     <div>
-                        <label htmlFor="">ImageURL</label>
-                        <input type="text" name="" id="" className='border w-full py-2 pl-2' placeholder='Service ImageURL' />
+                        <label htmlFor="">Provider Name</label>
+                        <input type="text" name="" id="" className='border w-full py-2 pl-2' placeholder='Provider Name' />
                     </div>
 
                     <div>
-                        <label htmlFor="">Description</label>
-                        <textarea type="text" name="" id="" rows="5" className='border w-full py-2 pl-2' placeholder='Service Description' />
+                        <label htmlFor="">Provider Email</label>
+                        <input type="text" name="" id="" className='border w-full py-2 pl-2' placeholder='Provider Email' />
+                    </div>
+
+                    <div>
+                        <label htmlFor="">Current User Name</label>
+                        <input type="text" name="" id="" rows="5" className='border w-full py-2 pl-2' placeholder='Current User Name' />
+                    </div>
+
+                    <div>
+                        <label htmlFor="">Current User Email</label>
+                        <input type="text" name="" id="" rows="5" className='border w-full py-2 pl-2' placeholder='Current User Email' />
                     </div>
 
                     <button className='border border-teal-500 px-5 py-3 text-teal-500 hover:text-white hover:bg-teal-500 w-full'>Book Service</button>
