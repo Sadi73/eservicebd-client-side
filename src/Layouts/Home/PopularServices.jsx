ffe30a65876cb12191cbaa3c431fa277fefac534
Sadi Muhammad Tanzim <saditanzim@gmail.com>
2024-05-11 11:28:09 +0600

    all service design added

--- src/Layouts/Home/PopularServices.jsx
+++ src/Layouts/Home/PopularServices.jsx
@@ -1,30 +1,26 @@
 import React from 'react';
-import { Card } from 'antd';
 import { useNavigate } from 'react-router-dom';
-const { Meta } = Card;
 
 const PopularServices = () => {
     const navigate = useNavigate();
 
     return (
-        <div className='my-10'>
-            <h1 className='text-3xl font-semibold text-center'>Popular Services</h1>
+        <div className='my-10 bg-teal-200 py-10'>
+            <h1 className='text-3xl font-semibold text-center text-teal-700'>Popular Services</h1>
             <p className='text-center'>As a customer focused solution provider, we are dedicated to redefine the home improvement service experience for all users</p>
 
-            <div className='card-container'>
-                <Card
-                    hoverable
-                    style={{
-                        width: 240,
-                    }}
-                    cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
-                >
-                    <Meta title="Europe Street beat" description="www.instagram.com" />
+            <div className='card-container w-[80%] mx-auto mt-10'>
+
+                <div className='w-72 pb-5 bg-white text-center hover:shadow-2xl'>
+                    <img src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" alt=""  className='w-full h-[50%]'/>
+                    <h1 className='text-teal-500 text-2xl'>Service Name</h1>
+                    <p>Description</p>
                     <button
-                        className='border border-teal-500 text-teal-500 px-3 py-2'
+                        className=' bg-teal-500 text-white px-3 py-2'
                         onClick={() => navigate('/service/1')}
                     >Details</button>
-                </Card>
+
+                </div>
 
             </div>
 
--- src/Layouts/Services/AllService.jsx
+++ src/Layouts/Services/AllService.jsx
@@ -1,9 +1,38 @@
+import { Avatar } from 'antd';
 import React from 'react';
 
 const AllService = () => {
     return (
-        <div>
-            AllService
+        <div className='my-10 px-10'>
+            <div className='flex gap-10 border border-teal-500 p-3 shadow-xl rounded-xl'>
+                <img
+                    src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
+                    alt=""
+                    className='w-96'
+                />
+
+                <div className='service details w-[500px] flex flex-col'>
+                    <h1>Name</h1>
+                    <p>Description</p>
+
+                </div>
+
+                <div className='service provider info w-[300px]'>
+                    <div className='flex items-center gap-3'>
+                        <Avatar />
+                        <h1>Name</h1>
+                    </div>
+                    <p>Email</p>
+
+                </div>
+
+                <div className='buttons flex items-center'>
+                    <div className=' flex flex-col gap-5'>
+                        <button className='bg-teal-500 text-white px-5 py-3 hover:bg-teal-700'>Details</button>
+                        <button className='bg-teal-500 text-white px-5 py-3 hover:bg-teal-700'>Book Now</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
