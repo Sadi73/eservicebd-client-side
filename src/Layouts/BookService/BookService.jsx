10097470a0e53bd53cd75934172e12683628329d
Sadi Muhammad Tanzim <saditanzim@gmail.com>
2024-05-12 08:30:14 +0600

    handled form values using formik

--- src/Layouts/BookService/BookService.jsx
+++ src/Layouts/BookService/BookService.jsx
@@ -1,7 +1,32 @@
 import React from 'react';
 import { Modal } from 'antd';
+import { useFormik } from 'formik';
 
 const BookService = ({ isModalOpen, setIsModalOpen }) => {
+
+    const { values, setValues, handleBlur, handleChange, handleSubmit, errors, touched } = useFormik({
+        initialValues: {
+            serviceId: '',
+            serviceTitle: '',
+            price: '',
+            imageURL: '',
+            providerInfo: {
+                providerName: '',
+                providerEmail: '',
+                providerImageURL: '',
+            },
+            customerInfo: {
+                customerName: '',
+                customerEmail: '',
+            },
+            status: 'Pending'
+
+        },
+        onSubmit: values => {
+            // Handle form submission here
+            console.log(values)
+        },
+    });
 
     const handleOk = () => {
         setIsModalOpen(false);
@@ -20,48 +45,117 @@ const BookService = ({ isModalOpen, setIsModalOpen }) => {
                 footer={null}
                 width={700}
             >
-                <form className=' mx-auto space-y-3 my-10'>
+                <form onSubmit={handleSubmit} className=' mx-auto space-y-3 my-10'>
                     <div>
                         <label htmlFor="">Service ID</label>
-                        <input type="text" name="" id="" className='border w-full py-2 pl-2' placeholder='Service ID' />
+                        <input
+                            type="text"
+                            name="serviceId"
+                            value={values?.serviceId}
+                            id=""
+                            onChange={handleChange}
+                            className='border w-full py-2 pl-2'
+                            placeholder='Service ID'
+                        />
                     </div>
 
                     <div>
                         <label htmlFor="">Service Name</label>
-                        <input type="text" name="" id="" className='border w-full py-2 pl-2' placeholder='Service Title' />
+                        <input
+                            type="text"
+                            name="serviceTitle"
+                            value={values?.serviceTitle}
+                            id=""
+                            onChange={handleChange}
+                            className='border w-full py-2 pl-2'
+                            placeholder='Service Title'
+                        />
                     </div>
 
                     <div>
                         <label htmlFor="">Service ImageURL</label>
-                        <input type="text" name="" id="" className='border w-full py-2 pl-2' placeholder='Service ImageURL' />
+                        <input
+                            type="text"
+                            name="imageURL"
+                            value={values?.imageURL}
+                            id=""
+                            onChange={handleChange}
+                            className='border w-full py-2 pl-2'
+                            placeholder='Service ImageURL'
+                        />
                     </div>
 
                     <div>
                         <label htmlFor="">Price</label>
-                        <input type="text" name="" id="" className='border w-full py-2 pl-2' placeholder='Service Price' />
+                        <input
+                            type="text"
+                            name="price"
+                            value={values?.price}
+                            id=""
+                            onChange={handleChange}
+                            className='border w-full py-2 pl-2'
+                            placeholder='Service Price'
+                        />
                     </div>
 
                     <div>
                         <label htmlFor="">Provider Name</label>
-                        <input type="text" name="" id="" className='border w-full py-2 pl-2' placeholder='Provider Name' />
+                        <input
+                            type="text"
+                            name="providerInfo.providerName"
+                            value={values?.providerInfo?.providerName}
+                            onChange={handleChange}
+                            id=""
+                            className='border w-full py-2 pl-2'
+                            placeholder='Provider Name'
+                        />
                     </div>
 
                     <div>
                         <label htmlFor="">Provider Email</label>
-                        <input type="text" name="" id="" className='border w-full py-2 pl-2' placeholder='Provider Email' />
+                        <input
+                            type="text"
+                            name="providerInfo.providerEmail"
+                            value={values?.providerInfo?.providerEmail}
+                            onChange={handleChange}
+                            id=""
+                            className='border w-full py-2 pl-2'
+                            placeholder='Provider Email'
+                        />
                     </div>
 
                     <div>
                         <label htmlFor="">Current User Name</label>
-                        <input type="text" name="" id="" rows="5" className='border w-full py-2 pl-2' placeholder='Current User Name' />
+                        <input
+                            type="text"
+                            name="customerInfo.customerName"
+                            value={values?.customerInfo?.customerName}
+                            onChange={handleChange}
+                            id=""
+                            rows="5"
+                            className='border w-full py-2 pl-2'
+                            placeholder='Current User Name'
+                        />
                     </div>
 
                     <div>
                         <label htmlFor="">Current User Email</label>
-                        <input type="text" name="" id="" rows="5" className='border w-full py-2 pl-2' placeholder='Current User Email' />
+                        <input
+                            type="text"
+                            name="customerInfo.customerEmail"
+                            value={values?.customerInfo?.customerEmail}
+                            onChange={handleChange}
+                            id=""
+                            rows="5"
+                            className='border w-full py-2 pl-2'
+                            placeholder='Current User Email'
+                        />
                     </div>
 
-                    <button className='border border-teal-500 px-5 py-3 text-teal-500 hover:text-white hover:bg-teal-500 w-full'>Book Service</button>
+                    <button
+                        type='submit'
+                        className='border border-teal-500 px-5 py-3 text-teal-500 hover:text-white hover:bg-teal-500 w-full'
+                    >Book Service</button>
                 </form>
             </Modal>
         </>
--- src/Layouts/Services/AddNewService.jsx
+++ src/Layouts/Services/AddNewService.jsx
@@ -1,5 +1,7 @@
 import { Select } from 'antd';
-import React from 'react';
+import { useFormik } from 'formik';
+import React, { useContext } from 'react';
+import { AuthContext } from '../../Providers/AuthProvider';
 
 const selectOptions = [
     {
@@ -45,6 +47,29 @@ const selectOptions = [
 ]
 
 const AddNewService = () => {
+
+    const { user } = useContext(AuthContext);
+
+    const { values, setValues, handleBlur, handleChange, handleSubmit, errors, touched } = useFormik({
+        initialValues: {
+            serviceTitle: '',
+            price: '',
+            imageURL: '',
+            serviceArea: '',
+            description: '',
+            providerInfo: {
+                providerName: user?.displayName,
+                providerEmail: user?.email,
+                providerImageURL: user?.imageURL,
+            }
+
+        },
+        onSubmit: values => {
+            // Handle form submission here
+            console.log(values)
+        },
+    });
+
     return (
         <div>
             <div className='h-96 bg-teal-500'></div>
@@ -52,38 +77,78 @@ const AddNewService = () => {
             <h1 className='text-5xl font-semibold text-center mt-10'>Add New Service</h1>
             <p className='text-center'>Adding new product has never been this easy</p>
 
-            <form className='w-[600px]  mx-auto space-y-3 my-10'>
+            <form
+                onSubmit={handleSubmit}
+                className='w-[600px]  mx-auto space-y-3 my-10'
+            >
                 <div>
                     <label htmlFor="">Service Title</label>
-                    <input type="text" name="" id="" className='border w-full py-2 pl-2' placeholder='Service Title' />
+                    <input
+                        type="text"
+                        name="serviceTitle"
+                        onChange={handleChange}
+                        value={values?.serviceTitle}
+                        id=""
+                        className='border w-full py-2 pl-2'
+                        placeholder='Service Title'
+                    />
                 </div>
 
                 <div>
                     <label htmlFor="">Price</label>
-                    <input type="text" name="" id="" className='border w-full py-2 pl-2' placeholder='Service Price' />
+                    <input
+                        type="text"
+                        name="price"
+                        onChange={handleChange}
+                        value={values?.price}
+                        id=""
+                        className='border w-full py-2 pl-2'
+                        placeholder='Service Price'
+                    />
                 </div>
 
                 <div>
                     <label htmlFor="">Service Area</label>
                     <Select
+                        name='serviceArea'
                         className='w-full'
                         placeholder='Service Area'
                         // onChange={handleChange}
+                        onChange={(e) => setValues({ ...values, serviceArea: e })}
                         options={selectOptions}
                     />
                 </div>
 
                 <div>
                     <label htmlFor="">ImageURL</label>
-                    <input type="text" name="" id="" className='border w-full py-2 pl-2' placeholder='Service ImageURL' />
+                    <input
+                        type="text"
+                        name="imageURL"
+                        onChange={handleChange}
+                        value={values?.imageURL}
+                        id=""
+                        className='border w-full py-2 pl-2'
+                        placeholder='Service ImageURL'
+                    />
                 </div>
 
                 <div>
                     <label htmlFor="">Description</label>
-                    <textarea type="text" name="" id="" rows="5" className='border w-full py-2 pl-2' placeholder='Service Description' />
+                    <textarea
+                        type="text"
+                        name="description"
+                        onChange={handleChange}
+                        value={values?.description}
+                        id=""
+                        rows="5"
+                        className='border w-full py-2 pl-2'
+                        placeholder='Service Description'
+                    />
                 </div>
 
-                <button className='border border-teal-500 px-5 py-3 text-teal-500 hover:text-white hover:bg-teal-500 w-full'>Add Service</button>
+                <button type='submit'
+                    className='border border-teal-500 px-5 py-3 text-teal-500 hover:text-white hover:bg-teal-500 w-full'
+                >Add Service</button>
             </form>
         </div>
     );
