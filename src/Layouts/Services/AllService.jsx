3e68c1618125c711ebff305d9ff5148db2c4bf99
Sadi Muhammad Tanzim <saditanzim@gmail.com>
2024-05-12 22:20:43 +0600

    get all service api added

--- src/Layouts/Services/AllService.jsx
+++ src/Layouts/Services/AllService.jsx
@@ -1,43 +1,53 @@
 import { Avatar, Modal } from 'antd';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import BookService from '../BookService/BookService';
 
 const AllService = () => {
 
     const [isModalOpen, setIsModalOpen] = useState(false);
+    const [allServices, setAllServices] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:3000/services/all')
+            .then(res => res.json())
+            .then(data => setAllServices(data))
+    }, []);
 
     return (
         <div className='my-10 px-10'>
-            <div className='flex gap-10 border border-teal-500 p-3 shadow-xl rounded-xl'>
-                <img
-                    src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
-                    alt=""
-                    className='w-96'
-                />
+            {allServices.map(service =>
+                <div key={service?._id} className='flex gap-10 border border-teal-500 p-3 shadow-xl rounded-xl mb-5'>
+                    <img
+                        src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
+                        alt=""
+                        className='w-96'
+                    />
 
-                <div className='service details w-[500px] flex flex-col'>
-                    <h1>Name</h1>
-                    <p>Description</p>
-
-                </div>
-
-                <div className='service provider info w-[300px]'>
-                    <div className='flex items-center gap-3'>
-                        <Avatar />
+                    <div className='service details w-[500px] flex flex-col'>
                         <h1>Name</h1>
-                    </div>
-                    <p>Email</p>
+                        <p>Description</p>
 
-                </div>
+                    </div>
 
-                <div className='buttons flex items-center'>
-                    <div className=' flex flex-col gap-5'>
-                        <Link to="/service/1"><button className='bg-teal-500 text-white px-5 py-3 hover:bg-teal-700'>Details</button></Link>
-                        <button className='bg-teal-500 text-white px-5 py-3 hover:bg-teal-700' onClick={() => setIsModalOpen(true)}>Book Now</button>
+                    <div className='service provider info w-[300px]'>
+                        <div className='flex items-center gap-3'>
+                            <Avatar />
+                            <h1>Name</h1>
+                        </div>
+                        <p>Email</p>
+
+                    </div>
+
+                    <div className='buttons flex items-center'>
+                        <div className=' flex flex-col gap-5'>
+                            <Link to="/service/1"><button className='bg-teal-500 text-white px-5 py-3 hover:bg-teal-700'>Details</button></Link>
+                            <button className='bg-teal-500 text-white px-5 py-3 hover:bg-teal-700' onClick={() => setIsModalOpen(true)}>Book Now</button>
+                        </div>
                     </div>
                 </div>
-            </div>
+            )
+            }
 
             {isModalOpen &&
                 <BookService
